784f894dd2d234c3ccc4638e63fa7e7020851672
cy925323@ohio.edu <cy925323@ohio.edu>
2025-07-23 16:38:53 -0400

    started initilization of weights and biases.

--- EVALUATORS/NeuralNetowrk/NeuralNetowrkOperator.cc
+++ EVALUATORS/NeuralNetowrk/NeuralNetowrkOperator.cc
@@ -1,3 +1,41 @@
 #include "NeuralNetworkOperator.h"
+#include <fstream>
+#include <iostream>
+#include <cmath>
+#include <random>
 
 float forwardPass(const InputTensor& input);
+
+void initializeBiases(const std::string& fileName, size_t numBiases){
+    std::ofstream outputStream;
+    outputStream.open(fileName);
+
+    if (!outputStream.is_open()){std::cout << "[0][DEBUG][NeuralNetworkOperator::resetBiases]\n"; return;}
+
+    for (int i = 0; i < numBiases; i++)
+        {
+            outputStream << "0\n";
+        }
+
+    outputStream.close();
+
+}
+
+void initializeWeightsRelu(const std::string& fileName, size_t inputNeurons, size_t outputNeurons){
+    std::ofstream outputStream;
+    outputStream.open(fileName);
+    size_t numWeights = inputNeurons * outputNeurons;
+
+    if (!outputStream.is_open()){std::cout << "[0][DEBUG][NeuralNetworkOperator::initializeWeightsRelu]\n"; return;}
+
+    // Initialize weights with a normal distribution:   w ~ N(0, sqrt(2/n_in))
+    float stddev = std::sqrt(2.0f / inputNeurons);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::normal_distribution<float> dis(0.0f, stddev);
+
+    for (size_t i = 0; i < numWeights; ++i) {
+        outputStream << dis(gen) << "\n";
+    }
+}
+
--- EVALUATORS/NeuralNetowrk/NeuralNetworkOperator.h
+++ EVALUATORS/NeuralNetowrk/NeuralNetworkOperator.h
@@ -1,8 +1,39 @@
-#include "../InputTensor.h"
+#include <string>
+#include "InputTensor.h"
 
 
 namespace NeuralNetworkOperator{
+
+    /*
+        Brief: Taked in a 768 neuron input tensor and forward passes thru a 512, 256, 128, 64 hidden layer then to a single output between 0 and 1. Closer to 1 means that white is winning
+
+        input: The input board tensor that will be evaluated
+    */
     float forwardPass(const InputTensor& input);
+
+
+    /*
+        Brief: resets all biases in a given file to 0
+    */
+    void initializeBiases(const std::string& fileName, size_t numBiases);
+
+
+    /*
+        Brief: Initilizes weights in a specified file using He initilization. Weights will mostly be initilized form +-0.15
+
+        fileName: The file to input these wieghts into
+        numWeights: The numbers of weights that need to be initialized
+    */
+   void initializeWeightsRelu(const std::string& fileName, size_t inputNeurons, size_t outputNeurons);
+
+    /*
+        Brief: initiliaizes weights in a specified file using Xavier initilization. Weights will be mostly initialized from +-0.304
+       
+        fileName: The file to input these wieghts into
+        numWeights: The numbers of weights that need to be initialized
+    */
+    void initializeWeightsSigmoid(const std::string& fileName, size_t inputNeurons,  size_t outputNeurons);
+
     
 
 }
